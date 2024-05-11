03d5d4a4e15e0a215c66020284410e350cf269f7
Ron <swansonofmaine@aol.com>
2024-05-11 08:53:17 -0400

    Add left/right movement

--- practice.py
+++ practice.py
@@ -8,6 +8,12 @@ class Square(pygame.sprite.Sprite):
         self.surf = pygame.Surface((25,25))
         self.surf.fill((0,200,255))
         self.rect = self.surf.get_rect()
+    def update(self,pressed_keys):
+        if pressed_keys[K_RIGHT] and self.rect.right < 800:
+            self.rect.move_ip(1,0)
+        if pressed_keys[K_LEFT] and self.rect.left > 0:
+            self.rect.move_ip(-1,0)
+
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 square = Square()
@@ -19,5 +25,8 @@ while gameOn:
                 gameOn = False
         elif event.type == QUIT:
             gameOn = False
+    pressed_keys = pygame.key.get_pressed()
+    square.update(pressed_keys)
+    screen.fill((255,255,255))
     screen.blit(square.surf,square.rect)
     pygame.display.flip()
